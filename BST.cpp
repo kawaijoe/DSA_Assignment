6f52d65468a8c856ce35f4e36d2bbc993a4404cb
Daviiddoo <s10173143@connect.np.edu.sg>
2018-01-14 14:28:51 +0800

    HI Hope this works

---
+++ BST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David on 14/01/2018.
+//
+
+#include "BST.h"
---
+++ BST.h
@@ -0,0 +1,14 @@
+//
+// Created by David on 14/01/2018.
+//
+
+#ifndef CODE_BST_H
+#define CODE_BST_H
+
+
+class BST {
+
+};
+
+
+#endif //CODE_BST_H
